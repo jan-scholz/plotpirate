dffe3be24de60d3fb8c125d76690cb1418755fa3
Jan Scholz <scholz_sfo@hotmail.com>
2017-09-20 22:11:51 -0400

    save figure, adjust background and other plot parameters for image saving

--- simulate_plots.py
+++ simulate_plots.py
@@ -7,11 +7,12 @@ import matplotlib.pyplot as plt
 from pylab import MaxNLocator
 from random import *
 
-bg = np.array([255, 241, 223])/255.0
+bgcolor = np.array([255, 241, 223])/255.0
 fill = np.array([224, 92, 128])/255.0
 linecolor = np.array([213, 208, 191])/255.0
 labelcolor = np.array([120, 117, 108])/255.0
 
+plt.rcParams['savefig.facecolor'] = bgcolor
 
 # parameters for random plot data
 N = randint(8, 12)
@@ -20,8 +21,10 @@ y_mu = gauss(0, 3)
 y_sd = expovariate(0.7)
 
 # parameterd for random plot style
-plotwidth = 3 + gauss(0, 0.5)
-plotheight = 2 + gauss(0, 0.5)
+plotwidth = 4 * 1.0
+plotheight = 3 * 1.0
+#plotwidth = 3 + gauss(0, 0.5)
+#plotheight = 2 + gauss(0, 0.5)
 barwidth_sd = plotwidth*2/N + gauss(0, 0.05)
 
 
@@ -29,8 +32,8 @@ X = np.linspace(start=x0, stop=x0+N-1, num=N).astype(int)
 Y = [gauss(y_mu, y_sd) for x in range(N)]
 
 
-fig = plt.figure(figsize=(5,3), facecolor=bg)
-ax = plt.subplot(1,1,1, facecolor=bg)
+fig = plt.figure(figsize=(plotwidth, plotheight), facecolor=bgcolor)
+ax = plt.subplot(1,1,1, facecolor=bgcolor)
 
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
@@ -65,7 +68,9 @@ ax.bar(X, Y, width=0.5, facecolor=fill, edgecolor=fill)
 print(X)
 print(Y)
 
-
-plt.show()
-
+#plt.show()
+plt.savefig('foo.png'
+    , dpi=60
+    , bbox_inches='tight')
+    #, pad_inches=0.5)
 
