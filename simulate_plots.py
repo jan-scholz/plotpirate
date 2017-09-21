21d691f5a278dae57fddeb960e27de89f7320682
Jan Scholz <scholz_sfo@hotmail.com>
2017-09-20 21:36:06 -0400

    simulate a simple bar plot
    similar to the ft japanese GDB plot

---
+++ simulate_plots.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import MaxNLocator
+from random import *
+
+bg = np.array([255, 241, 223])/255.0
+fill = np.array([224, 92, 128])/255.0
+linecolor = np.array([213, 208, 191])/255.0
+labelcolor = np.array([120, 117, 108])/255.0
+
+
+# parameters for random plot data
+N = randint(8, 12)
+x0 = randint(1950, 1995)
+y_mu = gauss(0, 3)
+y_sd = expovariate(0.7)
+
+# parameterd for random plot style
+plotwidth = 3 + gauss(0, 0.5)
+plotheight = 2 + gauss(0, 0.5)
+barwidth_sd = plotwidth*2/N + gauss(0, 0.05)
+
+
+X = np.linspace(start=x0, stop=x0+N-1, num=N).astype(int)
+Y = [gauss(y_mu, y_sd) for x in range(N)]
+
+
+fig = plt.figure(figsize=(5,3), facecolor=bg)
+ax = plt.subplot(1,1,1, facecolor=bg)
+
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['left'].set_visible(False)
+
+ax.tick_params(colors=labelcolor)
+
+ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+ax.yaxis.tick_right()
+ax.yaxis.set_label_position("right")
+ax.yaxis.set_ticks_position('none')
+
+ax.set_axisbelow(True)
+ax.yaxis.grid(color=linecolor, linestyle=':', linewidth=1)
+
+ax.bar(X, Y, width=0.5, facecolor=fill, edgecolor=fill)
+
+
+# x-axis intercepting y=0
+# adjust y ticks, steps=2
+# add text boxes (title, subtitle, source)
+
+# figure out position of most extreme x values
+#   - move them inwards
+#   - abbreviate them 2014 -> 14
+
+
+# parametrize everything in external file (mplstyle?)
+
+print(X)
+print(Y)
+
+
+plt.show()
+
+
