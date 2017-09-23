a9f25842eb89c78cadfb9448b6c7bfc42e67d79e
Jan Scholz <scholz_sfo@hotmail.com>
2017-09-23 15:56:24 -0400

    refactor create plot code into function

--- simulate_plots.py
+++ simulate_plots.py
@@ -14,63 +14,52 @@ labelcolor = np.array([120, 117, 108])/255.0
 
 plt.rcParams['savefig.facecolor'] = bgcolor
 
-# parameters for random plot data
-N = randint(8, 12)
-x0 = randint(1950, 1995)
-y_mu = gauss(0, 3)
-y_sd = expovariate(0.7)
 
-# parameterd for random plot style
-plotwidth = 4 * 1.0
-plotheight = 3 * 1.0
-#plotwidth = 3 + gauss(0, 0.5)
-#plotheight = 2 + gauss(0, 0.5)
-barwidth_sd = plotwidth*2/N + gauss(0, 0.05)
+def create_random_bar_pot():
+    # parameters for random plot data
+    N = randint(2, 2)
+    x0 = randint(1950, 1995)
+    y_mu = gauss(6, 3)
+    y_sd = expovariate(0.7)
 
+    # parameterd for random plot style
+    plotwidth = 4 * 0.5
+    plotheight = 3 * 0.5
+    barwidth_sd = plotwidth*2/N + gauss(0, 0.05)
 
-X = np.linspace(start=x0, stop=x0+N-1, num=N).astype(int)
-Y = [gauss(y_mu, y_sd) for x in range(N)]
+    # random data
+    X = np.linspace(start=x0, stop=x0+N-1, num=N).astype(int)
+    Y = [gauss(y_mu, y_sd) for x in range(N)]
 
+    fig = plt.figure(figsize=(plotwidth, plotheight), facecolor=bgcolor)
+    ax = plt.subplot(1,1,1, facecolor=bgcolor)
 
-fig = plt.figure(figsize=(plotwidth, plotheight), facecolor=bgcolor)
-ax = plt.subplot(1,1,1, facecolor=bgcolor)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
 
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.spines['left'].set_visible(False)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 
-ax.tick_params(colors=labelcolor)
+    ax.tick_params(colors=labelcolor)
+    ax.yaxis.tick_right()
+    ax.yaxis.set_label_position("right")
+    ax.yaxis.set_ticks_position('none')
 
-ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.set_axisbelow(True)
+    ax.yaxis.grid(color=linecolor, linestyle=':', linewidth=1)
 
-ax.yaxis.tick_right()
-ax.yaxis.set_label_position("right")
-ax.yaxis.set_ticks_position('none')
+    ax.bar(X, Y, width=0.5, facecolor=fill, edgecolor=fill)
 
-ax.set_axisbelow(True)
-ax.yaxis.grid(color=linecolor, linestyle=':', linewidth=1)
-
-ax.bar(X, Y, width=0.5, facecolor=fill, edgecolor=fill)
-
-
-# x-axis intercepting y=0
-# adjust y ticks, steps=2
-# add text boxes (title, subtitle, source)
-
-# figure out position of most extreme x values
-#   - move them inwards
-#   - abbreviate them 2014 -> 14
+    return fig
 
 
-# parametrize everything in external file (mplstyle?)
+nFigs = 2
 
-print(X)
-print(Y)
-
-#plt.show()
-plt.savefig('foo.png'
-    , dpi=60
-    , bbox_inches='tight')
-    #, pad_inches=0.5)
+for i in range(nFigs):
+    fig = create_random_bar_pot()
+    plt.savefig(f"foo{i:02}.png"
+        , dpi=60
+        , bbox_inches='tight')
+        #, pad_inches=0.5)
 
